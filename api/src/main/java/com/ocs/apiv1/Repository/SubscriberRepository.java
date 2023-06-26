d03729d2e3778f05703708f3c3ffd63205ced89e
Eldemcihat <98899165+Eldemcihat@users.noreply.github.com>
2023-06-26 11:48:12 +0300

    Commit local changes

--- api/src/main/java/com/ocs/apiv1/Repository/SubscriberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ocs.apiv1.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ocs.apiv1.Models.Subscriber;
-
-public interface SubscriberRepository extends JpaRepository<Subscriber, Object> {
-
-}
